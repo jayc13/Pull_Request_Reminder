0c5bb105cb878ebdab5aa0ea6111fce02a13f474
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2019-04-18 16:17:24 -0300

    First stable version

---
+++ main.py
@@ -0,0 +1,3 @@
+import slack_pull_reminder
+
+slack_pull_reminder.cli()
---
+++ slack_pull_reminder.py
@@ -0,0 +1,107 @@
+import os
+import sys
+
+import requests
+from github3 import login
+
+POST_URL = 'https://slack.com/api/chat.postMessage'
+
+ignore = os.environ.get('IGNORE_WORDS')
+IGNORE_WORDS = [i.lower().strip() for i in ignore.split(',')] if ignore else []
+
+repositories = os.environ.get('REPOSITORIES')
+REPOSITORIES = [r.lower().strip() for r in repositories.split(',')] if repositories else []
+
+usernames = os.environ.get('USERNAMES')
+USERNAMES = [u.lower().strip() for u in usernames.split(',')] if usernames else []
+
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', 'peya_automation')
+
+try:
+    SLACK_API_TOKEN = os.environ['SLACK_API_TOKEN']
+    GITHUB_API_TOKEN = os.environ['GITHUB_API_TOKEN']
+    ORGANIZATION = os.environ['ORGANIZATION']
+except KeyError as error:
+    sys.stderr.write('Please set the environment variable {0}'.format(error))
+    sys.exit(1)
+
+INITIAL_MESSAGE = """\
+Hi! There's a few open pull requests you should take a \
+look at:
+
+"""
+
+
+def fetch_repository_pulls(repository):
+    pulls = []
+    for pull in repository.pull_requests():
+        if pull.state == 'open' and (not USERNAMES or pull.user.login.lower() in USERNAMES):
+            pulls.append(pull)
+    return pulls
+
+
+def is_valid_title(title):
+    lowercase_title = title.lower()
+    for ignored_word in IGNORE_WORDS:
+        if ignored_word in lowercase_title:
+            return False
+
+    return True
+
+
+def format_pull_requests(pull_requests, owner, repository):
+    lines = []
+
+    for pull in pull_requests:
+        if is_valid_title(pull.title):
+            creator = pull.user.login
+            line = '*[{0}/{1}]* <{2}|{3} - by {4}>'.format(
+                owner, repository, pull.html_url, pull.title, creator)
+            lines.append(line)
+
+    return lines
+
+
+def fetch_organization_pulls(organization_name):
+    """
+    Returns a formatted string list of open pull request messages.
+    """
+    client = login(token=GITHUB_API_TOKEN)
+    organization = client.organization(organization_name)
+    lines = []
+
+    for repository in organization.repositories():
+        if REPOSITORIES and repository.name.lower() not in REPOSITORIES:
+            continue
+        unchecked_pulls = fetch_repository_pulls(repository)
+        lines += format_pull_requests(unchecked_pulls, organization_name,
+                                      repository.name)
+
+    return lines
+
+
+def send_to_slack(text):
+    payload = {
+        'token': SLACK_API_TOKEN,
+        'channel': SLACK_CHANNEL,
+        'as_user': False,
+        'text': text
+    }
+
+    response = requests.post(POST_URL, data=payload)
+    answer = response.json()
+    if not answer['ok']:
+        print(answer)
+        raise Exception(answer['error'])
+
+
+def cli():
+    lines = fetch_organization_pulls(ORGANIZATION)
+    if lines:
+        text = INITIAL_MESSAGE + '\n'.join(lines)
+        print(text)
+        send_to_slack(text)
+
+
+if __name__ == '__main__':
+    cli()
